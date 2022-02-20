48f14fcdee592fee323322225b876ec03ce5b021
root <root@DESKTOP-BRROTRE.localdomain>
2022-02-20 14:26:37 -0600

    delete unnecessary overhead

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    hi
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
