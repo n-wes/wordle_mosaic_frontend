0ecf874bd9659ae77f28a8901e8ccb46d774cbd1
root <root@DESKTOP-BRROTRE.localdomain>
2022-02-20 20:36:54 -0600

    init image preview

---
+++ src/components/ImagePreview.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div
+      class="imagePreviewWrapper"
+      :style="{ 'background-image': `url(${previewImage})` }"
+      @click="selectImage">
+    </div>
+    <input
+      ref="fileInput"
+      type="file"
+      @input="pickFile">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ImagePreview',
+    props: {
+        previewImage
+    }
+}
+</script>
